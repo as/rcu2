9f7a87a834d3483d7816528d1322b63052f4b5d0
as <as.utf8@gmail.com>
2018-10-29 18:02:29 -0700

    gofmt

--- example/example.go
+++ example/example.go
@@ -26,11 +26,11 @@ func main() {
 	for i, v := range conf {
 		s.Put(v.Zone, v.Service.ID, &conf[i])
 	}
-	
-	for _, z := range []string{"alpha", "bravo", "charlie"}{
+
+	for _, z := range []string{"alpha", "bravo", "charlie"} {
 		for _, id := range s.Keys(z) {
 			v, _ := s.Get(z, id).(*Config)
-			if v == nil{
+			if v == nil {
 				fmt.Println("ERR", z, id, "not found")
 				continue
 			}
